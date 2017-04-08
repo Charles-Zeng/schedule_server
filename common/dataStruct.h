3285fcc0e17b7c22cf4040fc2c6a2ffaaad59022
xiechongzhu <2936281@qq.com>
2017-04-08 20:56:49 +0800

    11

--- common/dataStruct.h
+++ common/dataStruct.h
@@ -21,4 +21,25 @@ struct ImageInfo
 	std::string templateId;
 };
 
+
+typedef enum _httpType
+{
+	E_HTTP_UPLOAD_IMAGE
+}E_HTTP_TYPE;
+
+struct HttpRequest
+{
+	void *connection;
+	E_HTTP_TYPE httpType;
+	std::string httpBody;
+};
+
+struct httpResponse
+{
+	void *connection;
+	E_HTTP_TYPE httpType;
+	std::string httpBody;
+	bool bSuccess;
+};
+
 #endif // _DATA_STRUCT_H_
